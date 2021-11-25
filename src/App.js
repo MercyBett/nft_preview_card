5203986057fa279c732bf6a36d07c983670a0951
Mercybett <merjep551@gmail.com>
2021-11-25 21:25:38 +0300

    map out end design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 function App() {
   return (
-    <div className="bg-blue-700 text-white">
-      <p>Let's create NFT!</p>
+    <div className="bg-blue-700 text-white min-h-screen items-center">
+      <div>
+        card cover
+        <div>image</div>
+        <div>content 1</div>
+        <div>Content 2</div>
+        <br />
+        <div>Content 3</div>
+      </div>
     </div>
   );
 }
