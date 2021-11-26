26cdd7f70ab0449854a9d27b7f229cce6f5c82ca
Mercybett <merjep551@gmail.com>
2021-11-26 11:49:25 +0300

    add card data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import equilibrium from "./img/equilibrium.jpg";
+
 function App() {
   return (
     <div className="bg-main text-white min-h-screen flex justify-center items-center">
-      <div className="bg-card rounded-lg">
-        <div>image</div>
-        <div className="font-Outfit">content 1</div>
-        <div>Content 2</div>
+      <div className="bg-card mx-6 p-6 rounded-lg w-100">
+        <div className="rounded-lg ">
+          <img src={equilibrium} alt="nft equilibrium" />
+        </div>
+        <div className="font-Outfit mt-4">
+          <p className="font-semibold">Equilibrium #3429</p>
+          <p>Our Equilibrium collection promotes balance and calm</p>
+        </div>
+        <div></div>
         <br />
         <div>Content 3</div>
       </div>
