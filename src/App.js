811eb3bbaea973c8e5e6293e0dbec34602de07bd
Mercybett <merjep551@gmail.com>
2021-11-27 10:03:34 +0300

    add svg and it's contents

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import equilibrium from "./img/equilibrium.jpg";
 import avatar from "./img/avatar.png";
+import ethereum from "./img/ethereum.svg";
+import view from "./img/view.svg";
+import clock from "./img/clock.svg";
+
 function App() {
   return (
-    <div className="bg-main text-white min-h-screen flex justify-center items-center">
+    <div className="bg-main text-sm text-white min-h-screen flex justify-center items-center">
       <div className="bg-card mx-6 p-6 rounded-lg w-screen md:w-60 lg:w-[300px] h-1/2 lg:my-20">
         <div className="h-1/2">
           <img className="rounded-lg" src={equilibrium} alt="nft equilibrium" />
@@ -11,9 +15,18 @@ function App() {
           <p className="font-semibold">Equilibrium #3429</p>
           <p>Our Equilibrium collection promotes balance and calm</p>
         </div>
-        <div></div>
-        <br />
-        <div className="flex flex-row items-center space-x-4">
+        <div className="my-4 flex flex-row justify-between">
+          <div className="flex flex-row space-x-2">
+            <img src={ethereum} alt="etherium svg" />
+            <p className="text-cyan">0.041 ETH</p>
+          </div>
+          <div className="flex flex-row space-x-2">
+            <img src={clock} alt="etherium svg" />
+            <p> 3 days left</p>
+          </div>
+        </div>
+        <hr />
+        <div className="mt-4 flex flex-row items-center space-x-4">
           <img className="w-1/4" src={avatar} alt="person" />
           <p className="text-sm">
             <span>Creation of</span> Julius Wyvern
