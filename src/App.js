8a3f02a9c91cd0ac78d8939309bf622b6fb7dfec
Mercybett <merjep551@gmail.com>
2021-11-27 18:35:15 +0300

    add hover effect to text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import clock from "./img/clock.svg";
 function App() {
   return (
     <div className="bg-main text-lg text-soft font-Outfit min-h-screen flex justify-center items-center">
-      <div className="bg-card cursor-pointer mx-6 p-5 rounded-lg w-screen md:w-60 lg:w-[300px] h-1/2 lg:my-20">
+      <div className="bg-card font-Output cursor-pointer mx-6 p-5 rounded-lg w-screen md:w-60 lg:w-[360px] h-1/2 lg:my-20">
         <div className="h-1/2">
           <img
             className="rounded-lg hover:bg-cyan"
@@ -15,11 +15,13 @@ function App() {
             alt="nft equilibrium"
           />
         </div>
-        <div className="font-Outfit mt-4">
+        <div className=" tracking-wide mt-4">
           <p className="font-semibold text-white hover:text-cyan mb-2">
             Equilibrium #3429
           </p>
-          <p>Our Equilibrium collection promotes balance and calm</p>
+          <p className="font-light">
+            Our Equilibrium collection promotes balance and calm
+          </p>
         </div>
         <div className="my-4 text-sm flex flex-row justify-between">
           <div className="flex flex-row space-x-2">
@@ -34,8 +36,9 @@ function App() {
         <hr className="text-line" />
         <div className="mt-4 flex flex-row items-center text-sm space-x-4">
           <img className="w-1/4" src={avatar} alt="person" />
-          <p className="text-white">
-            <span className="text-soft">Creation of</span> Julius Wyvern
+          <p className="text-soft">
+            Creation of
+            <span className="text-white hover:text-cyan"> Julius Wyvern</span>
           </p>
         </div>
       </div>
